0183eae9f4c614f891a7c77af1badf4035505fd9
Long Sun <long.sun@latitudefinancial.com>
2020-04-13 15:55:31 +1000

    Remove example codebases

--- src/main/java/com/kci/command/PipelineCreateCommand.java
+++ src/main/java/com/kci/command/PipelineCreateCommand.java
@@ -1,5 +1,7 @@
 package com.kci.command;
 
+import com.kci.service.PipelineCreateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
@@ -9,11 +11,14 @@ import java.util.concurrent.Callable;
         description = "Create pipeline from a template codebase"
 )
 public class PipelineCreateCommand implements Callable<Integer> {
-    private static final String TEMPLATE_CODEBASE = "https://github.com/spritesun/spring-boot-http-gradle-demo";
+    private static final String TEMPLATE_CODEBASE_URL = "https://github.com/spritesun/spring-boot-http-gradle-demo";
+
+    @Autowired
+    private PipelineCreateService service;
 
     @Override
     public Integer call() throws Exception {
-        service.createWithUri()
+        service.createWithUrl(TEMPLATE_CODEBASE_URL);
         return 0;
     }
 }
